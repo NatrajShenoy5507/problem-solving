bb7d0ef12328902f690d32d41a7cd2266d14d51f
NatrajShenoy5507 <natrajshenoy@gmail.com>
2026-01-10 11:50:32 +0530

    sliding window

--- problemsolving/problem-1.js
+++ problemsolving/problem-1.js
@@ -1,13 +1,22 @@
-let num =[2,1,5,1,3,2];
-let k =3;
-let max =0;
-let sum;
-for(let i =0; i<num.length - k; i++){
-    sum = num[i]+num[i+1]+num[i+2]
-    if(max < sum){
-        max = sum;
-    }else{
-        continue;
+ function addingNumbers(num, k){
+    let max =0;
+    let sum;
+    if(num.length < k){
+        return null
     }
-}
-console.log(max)
+    //first element sum
+    for(let i=0; i<k; i++){
+        max += num[i];
+    }
+    sum = max
+    //check for next elements
+    for(let i =k; i< num.length; i++){
+        max = max - num[i - k] + num[i];
+        sum = Math.max(sum, max);
+    }
+    return sum
+ }
+ let num =[2,1,5,1,3,2];
+ let k =4;
+ const maxiumSum= addingNumbers(num, k)
+ console.log(maxiumSum)
