b5bc7badf61eadc67da1a5c8974ee87c8cf2a9e2
NatrajShenoy5507 <natrajshenoy@gmail.com>
2026-01-10 11:57:55 +0530

    fibonacci

--- problemsolving/fibonacci.js
+++ problemsolving/fibonacci.js
@@ -2,10 +2,17 @@ function fibbonaci(number){
     let n1=0
     let n2=1
     let sum =0
+    console.log(n1);
+    console.log(n2);
     for (let index = 0; index < number; index++) {
-        sum =n1+n2
+        sum = n1+n2;
+        // console.log(sum);
         n1=n2
         n2=sum 
+        if(sum > number){
+            break
+        }
+        console.log(sum);
     }
 }
-fibbonaci(9)
+fibbonaci(50)
