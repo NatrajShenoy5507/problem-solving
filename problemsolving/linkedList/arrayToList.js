f1775c25e3ef80e9c91200ef893cfdd44f0995c4
NatrajShenoy5507 <natrajshenoy@gmail.com>
2026-05-30 19:26:30 +0530

    changes

--- problemsolving/linkedList/arrayToList.js
+++ problemsolving/linkedList/arrayToList.js
@@ -1,11 +1,15 @@
 import List from "./linkedList.js";
 
-let arr =[1,2,3,4,5,6,7,8,9,0];
-let head = new List(arr[0]);
-let current = head;
-
-for(let i=1; i< arr.length; i++){
-    current.next = new List(arr[i]);
-    current =current.next;
+function createList(arr){
+    let head = new List(arr[0]);
+    let current = head;
+    
+    for(let i=1; i< arr.length; i++){
+        current.next = new List(arr[i]);
+        current =current.next;
+    }
+ return head;
 }
-console.log(head) 
+let arr =[1,2,3,4,5,6,7,8,9,0];
+let finalList = createList(arr);
+console.log(finalList)
